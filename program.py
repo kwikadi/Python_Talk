c8a0e8ba79090cf169ab983ba862414d215c7784
kwikadi <kwikadi@gmail.com>
2015-01-15 21:30:13 +0530

    more features introduced

--- program.py
+++ program.py
@@ -3,8 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	return 'Hello World part 2!'
+
+@app.route('/username/<identity>')
+def print_id(identity):
+	return 'Hello %s' % identity
+
+@app.route('/user_id/<int:identity>')
+def print_id_int(identity):
+	return 'Hello robot %d' %identity
+
+@app.route('/hello')
+@app.route('/goodbye')
+def annoy():
+	return 'what what'
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+	app.run(debug = True)
