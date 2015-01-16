e3cf2d048753806b6f492c45582d989fa315335c
kwikadi <kwikadi@gmail.com>
2015-01-16 13:03:38 +0530

    render_temp, request etc

--- program.py
+++ program.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import (
+							Flask,
+							render_template,
+							request
+						)
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World part 2!'
+	return render_template('index.html')
 
-@app.route('/username/<identity>')
-def print_id(identity):
-	return 'Hello %s' % identity
+@app.route('/generate')
+def generate_link():
+	field = request.args.get('Field')
+	return render_template('rel_info.html', data = field)
 
 @app.route('/user_id/<int:identity>')
 def print_id_int(identity):
