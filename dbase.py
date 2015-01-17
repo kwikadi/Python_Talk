8fdd7ebd6c5a31376b7e0d8d911a299aba540101
Shadab Zafar <dufferzafar0@gmail.com>
2015-01-17 11:18:41 +0530

    Database now seems to be working

--- dbase.py
+++ dbase.py
@@ -28,35 +28,54 @@ def read(sql, values=None):
 
 
 def insert_values():
-    con.execute('''INSERT INTO LINKS (NAME, FIELD) VALUES
-            ('http://www.theverge.com', 'Computers'),
-            ('http://www.wired.com', 'Music'),
-            ('http://www.engadget.com', 'English'),
-            ('http://www.reddit.com', 'English'),
-            ('http://www.kickass.so', 'Computers'),
-            ('http://www.thepiratebay.org', 'Maths'),
-            ('http://www.youtube.com', 'Maths'),
-            ('http://www.facebook.com', 'Science'),
-            ('http://www.thisismynext.com', 'Science'),
-            ('http://www.windowsphone.com', 'Music'),
-            ('http://www.amazinglink.com', 'Computers'),
-            ('http://www.randomsite.com', 'Science'),
-            ('http://www.wowowow.com', 'English'),
-            ('http://www.random.org', 'Maths'),
-            ('http://www.samsung.com', 'Philosophy'),
-            ('http://www.nokia.com', 'Philosophy'),
-            ('http://www.gmail.com', 'Philosophy'),
-            ('http://www.google.com', 'Maths'),
-            ('http://www.thenextweb.com', 'Computers'),
-            ('http://www.yahoo.com', 'Philosophy'),
-            ('http://www.bangbang.com', 'English'),
-            ('http://www.lalloo.com', 'Music'),
-            ('http://www.digg.com', 'Music'),
-            ('http://www.github.com', 'Science'); ''')
+    cur = con.cursor()
+
+    cur.execute(
+        '''
+        INSERT INTO LINKS (NAME, FIELD) VALUES
+        ('http://www.theverge.com', 'Computers'),
+        ('http://www.wired.com', 'Music'),
+        ('http://www.engadget.com', 'English'),
+        ('http://www.reddit.com', 'English'),
+        ('http://www.kickass.so', 'Computers'),
+        ('http://www.thepiratebay.org', 'Maths'),
+        ('http://www.youtube.com', 'Maths'),
+        ('http://www.facebook.com', 'Science'),
+        ('http://www.thisismynext.com', 'Science'),
+        ('http://www.windowsphone.com', 'Music'),
+        ('http://www.amazinglink.com', 'Computers'),
+        ('http://www.randomsite.com', 'Science'),
+        ('http://www.wowowow.com', 'English'),
+        ('http://www.random.org', 'Maths'),
+        ('http://www.samsung.com', 'Philosophy'),
+        ('http://www.nokia.com', 'Philosophy'),
+        ('http://www.gmail.com', 'Philosophy'),
+        ('http://www.google.com', 'Maths'),
+        ('http://www.thenextweb.com', 'Computers'),
+        ('http://www.yahoo.com', 'Philosophy'),
+        ('http://www.bangbang.com', 'English'),
+        ('http://www.lalloo.com', 'Music'),
+        ('http://www.digg.com', 'Music'),
+        ('http://www.github.com', 'Science');
+        '''
+    )
+
+    con.commit()
 
 
 def create_tables():
-    con.execute('''CREATE TABLE IF NOT EXISTS LINKS
-           (ID  INTEGER PRIMARY KEY AUTOINCREMENT,
-           NAME VARCHAR(200)    NOT NULL,
-           FIELD    VARCHAR(20) NOT NULL);''')
+    cur = con.cursor()
+
+    cur.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS LINKS (
+
+            ID          INTEGER         PRIMARY KEY AUTOINCREMENT,
+
+            NAME        VARCHAR(200)    NOT NULL,
+
+            FIELD       VARCHAR(20)     NOT NULL);
+        '''
+    )
+
+    con.commit()
